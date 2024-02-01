fef6d5ece9c3c8406ab458386a4b8f2dc6121da4
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-01 15:09:59 +0300

    Update personalInfo.controller.js with user ID and avatar field

--- backend/controllers/personalInfo.controller.js
+++ backend/controllers/personalInfo.controller.js
@@ -45,6 +45,7 @@ const createPersonalInformation = asyncHandler(async (req, res) => {
       address,
       city,
       state,
+      userId: req.user._id,
     });
 
     if (!personalInformation) {
@@ -61,7 +62,7 @@ const createPersonalInformation = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update personal information
-// @route   PUT /api/personals/:id
+// @route   PUT /api/personal-info/:id
 // @access  Private
 const updatePersonalInformation = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, phone, address, city, state } = req.body;
@@ -81,7 +82,8 @@ const updatePersonalInformation = asyncHandler(async (req, res) => {
     !phone &&
     !address &&
     !city &&
-    !state
+    !state &&
+    !req.file
   ) {
     return res
       .status(400)
@@ -89,26 +91,32 @@ const updatePersonalInformation = asyncHandler(async (req, res) => {
   }
 
   try {
-    const updatedInformation = await PersonalInformation.findOneAndUpdate(
-      { _id: req.params.id },
-      {
-        firstName,
-        lastName,
-        email,
-        phone,
-        address,
-        city,
-        state,
-      },
-      { new: true }
+    const personalInformation = await PersonalInformation.findById(
+      req.params.id
     );
 
-    if (!updatedInformation) {
+    if (!personalInformation) {
       return res.status(404).json({
-        message: 'Unable to update Personal information',
+        message: 'Personal information not found',
         status: false,
       });
     }
+
+    if (personalInformation.userId?.toString() !== req.user._id?.toString()) {
+      return res.status(401).json({ message: 'Unauthorized', status: false });
+    }
+
+    console.log(req.file);
+    if (firstName) personalInformation.firstName = firstName;
+    if (lastName) personalInformation.lastName = lastName;
+    if (email) personalInformation.email = email;
+    if (phone) personalInformation.phone = phone;
+    if (address) personalInformation.address = address;
+    if (city) personalInformation.city = city;
+    if (state) personalInformation.state = state;
+    if (req.file) personalInformation.avatar = req.file.path;
+
+    const updatedInformation = await personalInformation.save();
 
     res.status(200).json({ updatedInformation, status: true });
   } catch (error) {
