3031565445941a7779e05d6e34cbce8ba54a4787
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-01 20:26:38 +0300

    Update Footer styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = ({ theme }) => {
     <footer
       className={`bg-${
         theme === 'dark' ? 'backgroundDark' : 'backgroundLight'
-      } absolute bottom-0 w-full h-16 flex flex-wrap justify-center items-center px-10`}
+      } sticky w-full h-16 flex flex-wrap justify-center items-center px-10 bg-gray-500 text-backgroundLight`}
     >
       <p>Footer</p>
     </footer>
