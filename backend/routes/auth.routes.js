4686189b305e220be5f8703c7c084e7c779ddfc7
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-01 15:09:51 +0300

    Add file upload functionality for profile and resume pictures

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -9,6 +9,18 @@ const {
   deleteMe,
 } = require('../controllers/auth.controller');
 const protect = require('../middlewares/authMiddleware');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/profile-pictures/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().getTime() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.route('/register').post(registerMe);
 router.route('/login').post(logMeIn);
@@ -16,7 +28,7 @@ router.route('/logout').get(protect, logMeOut);
 router
   .route('/profile/:id')
   .get(protect, getMe)
-  .put(protect, updateMe)
+  .put(protect, upload.single('avatar'), updateMe)
   .delete(protect, deleteMe);
 
 module.exports = router;
--- backend/routes/personalInformation.routes.js
+++ backend/routes/personalInformation.routes.js
@@ -7,10 +7,24 @@ const {
   updatePersonalInformation,
   deletePersonalInformation,
 } = require('../controllers/personalInfo.controller');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/resume-pictures/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().getTime() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.route('/').get(getPersonalInformation);
 router.route('/').post(protect, createPersonalInformation);
-router.route('/:id').put(protect, updatePersonalInformation);
+router
+  .route('/:id')
+  .put(protect, upload.single('avatar'), updatePersonalInformation);
 router.route('/:id').delete(protect, deletePersonalInformation);
 
 module.exports = router;
