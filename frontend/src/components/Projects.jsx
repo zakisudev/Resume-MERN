f747765c4ad81c3e4b02e4cde9dfd5336bd6ffa7
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-04 21:43:34 +0300

    Add image preview functionality to Projects component

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,41 +1,108 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Projects = ({ projects, theme }) => {
+  const [isImageOpen, setIsImageOpen] = useState(false);
+  const [image, setImage] = useState({});
+
+  const handleImageClick = (img) => {
+    setIsImageOpen(true);
+    setImage(img);
+  };
+
+  const handleClose = () => {
+    setIsImageOpen(false);
+  };
   return (
-    <div className="my-2">
-      <h2 className="flex flex-row justify-center items-center text-2xl font-bold max-w-[80%] mx-auto">
-        Project:
-      </h2>
-      <ul className="flex flex-row justify-center items-center space-x-4 my-1">
-        {projects?.map((prj) => (
-          <li key={prj.id} className="flex flex-wrap gap-2">
-            <div className="flex">
+    projects && (
+      <div className="my-2 w-full text-left">
+        <h2 className="flex flex-row items-center text-2xl font-bold underline uppercase mb-2 bg-gray-300 py-2 px-1">
+          Project:
+        </h2>
+        <ul className="flex flex-col justify-center items-center my-1">
+          {projects?.project?.map((prj, index) => (
+            <li
+              key={prj._id}
+              className={`flex gap-2 w-full my-2 ${
+                index === projects?.project?.length - 1
+                  ? 'border-b-2 border-primaryColorLight pb-2'
+                  : 'border-b-2 border-gray-300'
+              }`}
+            >
               <img
-                src={prj?.image}
+                src={`/${prj?.image}`}
                 alt={prj.title}
-                className="w-24 h-24 object-cover"
+                className="w-24 h-24 object-cover cursor-pointer p-1"
+                onClick={() => handleImageClick(prj)}
               />
+
+              <div className="flex flex-col text-left w-full">
+                <h3 className="text-xl font-bold">{prj?.title}</h3>
+                <p className="text-sm text-gray-600">{prj?.description}</p>
+                <ul className="inline-flex gap-3">
+                  <>
+                    <span className="font-semibold underline underline-offset-1">
+                      Technologies used:{' '}
+                    </span>
+                    {prj?.technologies[0]
+                      ?.split(',')
+                      ?.map((technology, index) => {
+                        if (
+                          index ===
+                          prj?.technologies[0]?.split(',').length - 1
+                        ) {
+                          return <li key={index}>{technology.trim()}</li>;
+                        }
+                        return <li key={index}>{technology.trim()} /</li>;
+                      })}
+                  </>
+                </ul>
+                <Link
+                  to={prj?.link}
+                  className="text-blue-700 hover:underline w-fit"
+                  target="_blank"
+                >
+                  Visit demo
+                </Link>
+              </div>
+            </li>
+          ))}
+          {isImageOpen && (
+            <div className="fixed z-10 inset-0 overflow-y-auto">
+              <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div
+                  className="fixed inset-0 transition-opacity"
+                  aria-hidden="true"
+                >
+                  <div className="absolute inset-0 bg-black/40 opacity-75"></div>
+                </div>
+
+                <span
+                  className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-3xl sm:w-full">
+                  <img
+                    src={`/${image?.image}`}
+                    alt={image?.title}
+                    className="w-full h-full object-cover"
+                  />
+                  <button
+                    onClick={handleClose}
+                    className="p-1 bg-gray-300 font-semibold"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-center space-y-2">
-              <h3 className="text-xl font-bold">{prj?.title}</h3>
-              <p className="text-lg">{prj?.description}</p>
-              <p className="text-lg flex gap-1 justify-center items-center">
-                {prj?.technologies?.map((tech) => (
-                  <span key={tech}>{tech}</span>
-                ))}
-              </p>
-              <p className="text-lg">
-                {prj?.city}, {prj?.state}
-              </p>
-              {prj?.description && (
-                <p className="text-lg">{prj?.description}</p>
-              )}
-              <Link to={prj?.link}>Visit site</Link>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+          )}
+        </ul>
+      </div>
+    )
   );
 };
+
 export default Projects;
