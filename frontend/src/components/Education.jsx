2add90241f58c7871b9fa990ccb43434c43ad3c2
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-04 21:40:35 +0300

    Refactor Education component to handle nested education data

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -1,29 +1,32 @@
 const Education = ({ education }) => {
   return (
     <div className="my-2 w-full text-left">
-      <h2 className="flex flex-row items-center text-2xl font-bold underline">
+      <h2 className="flex flex-row items-center text-2xl font-bold underline uppercase mb-2 bg-gray-300 py-2 px-1">
         Education:
       </h2>
       <ul className="flex flex-col justify-center items-center space-y-2">
-        {education?.map((edu, index) => (
-          <li
-            key={edu?._id}
-            className={`flex flex-col w-full ${
-              index === education?.length - 1
-                ? 'border-b-2 border-primaryColorLight'
-                : 'border-b-2 border-gray-300'
-            }`}
-          >
-            <h3 className="text-xl font-bold italic">{edu?.schoolName}</h3>
-            <p className="text-md font-semibold text-gray-500 italic">
-              {edu?.degree} in {edu?.fieldOfStudy}
-            </p>
-            <p className="text-md">
-              {edu?.startYear} - {edu?.endYear}
-            </p>
-            {edu?.description && <p className="text-sm">{edu?.description}</p>}
-          </li>
-        ))}
+        {education &&
+          education?.Educations?.map((edu, index) => (
+            <li
+              key={edu?._id}
+              className={`flex flex-col w-full ${
+                index === education?.Educations?.length - 1
+                  ? 'border-b-2 border-primaryColorLight pb-2'
+                  : 'border-b-2 border-gray-300'
+              }`}
+            >
+              <h3 className="text-xl font-bold italic">{edu?.schoolName}</h3>
+              <p className="text-md font-semibold text-gray-500 italic">
+                {edu?.degree} in {edu?.fieldOfStudy}
+              </p>
+              <p className="text-md">
+                {edu?.startYear} - {edu?.endYear}
+              </p>
+              {edu?.description && (
+                <p className="text-sm">{edu?.description}</p>
+              )}
+            </li>
+          ))}
       </ul>
     </div>
   );
