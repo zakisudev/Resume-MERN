74fa6975a52a5d0af38b9f8a012b80b2e813894a
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-05 13:32:19 +0300

    Add logout functionality and update header links

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,24 +1,54 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import DarkModeToggle from '../DarkModeToggle';
+import { logout } from './../../utils/apis';
 
 const Header = ({ toggleTheme, theme }) => {
-  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  const user = localStorage.getItem('userInfo') ? true : false;
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      localStorage.removeItem('userInfo');
+      navigate('/', { replace: true });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <header className="sticky top-0 w-full h-16 flex flex-wrap gap-5 items-center px-10 bg-secondaryColorDark">
-      <Link
-        to="/"
-        className="px-2 py-1 rounded bg-primaryColorLight font-semibold"
-      >
-        Home
-      </Link>
-      {token && (
+      <div className="flex justify-between w-full flex-1">
         <Link
-          to="/profile"
-          className="top-5 right-5 p-1 bg-primaryColorLight rounded font-semibold "
+          to="/user"
+          className="px-2 py-1 rounded bg-primaryColorLight font-semibold"
         >
-          Profile
+          Home
         </Link>
-      )}
+        {user ? (
+          <div className="flex justify-center items-center gap-3">
+            <Link
+              to="/profile"
+              className="top-5 right-5 p-1 bg-primaryColorLight rounded font-semibold "
+            >
+              Profile
+            </Link>
+            <button
+              onClick={handleLogout}
+              className="top-5 right-5 p-1 bg-primaryColorLight rounded font-semibold ml-auto mr-32"
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <Link
+            to="/admin/login"
+            className="top-5 right-5 p-1 bg-primaryColorLight rounded font-semibold ml-auto mr-32"
+          >
+            Login
+          </Link>
+        )}
+      </div>
       <DarkModeToggle toggleTheme={toggleTheme} theme={theme} />
     </header>
   );
