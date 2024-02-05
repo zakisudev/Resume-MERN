0009c2a1b03068e4b293aaa5eba4882065e9db2b
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-05 13:26:30 +0300

    Refactor project update logic and handle error cases

--- backend/controllers/projects.controller.js
+++ backend/controllers/projects.controller.js
@@ -10,6 +10,12 @@ const getProjects = asyncHandler(async (req, res) => {
     const projects = await Project.findOne({
       userId: req.params.id,
     });
+    if (!projects) {
+      return res
+        .status(404)
+        .json({ message: 'Projects not found', status: false });
+    }
+
     res.status(200).json({ projects, status: true });
   } catch (error) {
     res.status(500).json({ message: error.message, status: false });
@@ -93,37 +99,61 @@ const createProject = asyncHandler(async (req, res) => {
 // @route   PUT /api/projects/:id
 // @access  Private
 const updateProject = asyncHandler(async (req, res) => {
-  const { title, description, link, image, technologies } = req.body;
+  const { title, description, link, technologies } = req.body;
 
   if (!req.user) {
-    res.status(401).json({ message: 'Unauthorized', status: false });
+    return res.status(401).json({ message: 'Not authorized', status: false });
   }
 
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    res.status(404).json({ message: 'Project not found', status: false });
+    return res
+      .status(404)
+      .json({ message: 'Invalid project id', status: false });
+  }
+
+  if (!title && !description && !link && !technologies && !req.file) {
+    return res
+      .status(400)
+      .json({ message: 'Please fill at least one field', status: false });
   }
 
   try {
-    const updatedProject = await Project.findOneAndUpdate(
-      { _id: req.params.id }, // filter
-      {
-        // update
-        title,
-        description,
-        link,
-        image,
-        technologies,
-      },
-      { new: true } // options
-    );
+    const prj = await Project.findOne({
+      userId: req.user._id,
+    });
+    const prjNames = prj.project.map((p) => p.title);
 
-    if (!updatedProject) {
-      res
-        .status(400)
-        .json({ message: 'Unable to update project', status: false });
+    if (!prj) {
+      return res
+        .status(404)
+        .json({ message: 'Project not found', status: false });
     }
 
-    res.status(200).json({ project: updatedProject, status: true });
+    const existingProject = prj.project.find(
+      (p) => p._id.toString() === req.params.id
+    );
+
+    if (prj && !existingProject) {
+      return res
+        .status(400)
+        .json({ message: 'Project not found', status: false });
+    }
+
+    if (prj && existingProject && prjNames.includes(title)) {
+      return res
+        .status(400)
+        .json({ message: 'Project already exists', status: false });
+    }
+
+    if (title) existingProject.title = title;
+    if (description) existingProject.description = description;
+    if (link) existingProject.link = link;
+    if (req.file) existingProject.image = req.file.path;
+    if (technologies) existingProject.technologies = technologies;
+
+    await prj.save();
+
+    res.status(200).json({ projects: prj, status: true });
   } catch (error) {
     res.status(400).json({ message: error.message, status: false });
   }
