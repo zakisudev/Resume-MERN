38381eea56add0d10e043c1ef8895d72b4ff978a
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-05 13:54:36 +0300

    Update server.js to serve static assets in production

--- backend/server.js
+++ backend/server.js
@@ -29,12 +29,13 @@ app.use('/uploads', express.static('./uploads/'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
   });
 } else {
-  app.get('/', (_, res) => {
+  app.get('/', (req, res) => {
     res.send('API is running...');
   });
 }
