0e2e15e993d2fb6e5ea47afbf29237f43bfdf140
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-05 13:28:18 +0300

    Refactor authMiddleware.js for user model

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,20 +1,22 @@
 const { verifyToken } = require('../utils/helpers');
-const Me = require('../models/me');
+const User = require('../models/user.model');
 
 const protect = async (req, res, next) => {
   const token = req.cookies?.jwt;
 
   if (!token) {
-    res.status(401).json({ message: 'Not authorized', status: false });
+    res.status(401).json({ message: 'Unauthorized, no token', status: false });
   }
 
   try {
     const decoded = verifyToken(token);
 
-    req.user = await Me.findById(decoded._id).select('-password');
+    req.user = await User.findById(decoded?._id).select('-password');
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Not authorized', status: false });
+    res
+      .status(401)
+      .json({ message: 'Unauthorized, invalid token', status: false });
   }
 };
 
