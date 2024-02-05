d66b0b39694da1db09d2f527032d2e697bcaa7b6
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-05 13:32:09 +0300

    Add user authentication and redirect to login page if user is not logged in

--- frontend/src/components/common/Layout.jsx
+++ frontend/src/components/common/Layout.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Header from './Header';
 import { useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = () => {
+  const navigate = useNavigate();
   const [theme, setTheme] = useState('light');
+  const user = localStorage.getItem('userInfo') ? true : false;
 
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
@@ -16,10 +18,16 @@ const Layout = () => {
     document.documentElement.setAttribute('data-theme', theme);
   }, [theme]);
 
+  useEffect(() => {
+    if (!user) {
+      navigate('/admin/login', { replace: true });
+    }
+  }, [user, navigate]);
+
   return (
     <div className="min-h-screen">
       <Header toggleTheme={toggleTheme} theme={theme} />
-      <main className="w-[80%] flex flex-col justify-center items-center mx-auto">
+      <main className="w-full flex flex-col justify-center items-center mx-auto h-full">
         <Outlet />
       </main>
       <ToastContainer
