150dad092965a0b0ea353c1d20c34d96f7acce9b
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-05 14:24:59 +0300

    Update API URLs to use localhost

--- frontend/src/constants/urls.js
+++ frontend/src/constants/urls.js
@@ -1,8 +1,8 @@
-export const AUTH_URL = '/api/auth';
-export const PERSONAL_INFO_URL = '/api/personal-info';
-export const SUMMARY_URL = '/api/summary';
-export const EXPERIENCE_URL = '/api/work-experiences';
-export const EDUCATION_URL = '/api/educations';
-export const SKILL_URL = '/api/skills';
-export const PROJECT_URL = '/api/projects';
-export const SOCIAL_URL = '/api/social-links';
+export const AUTH_URL = 'http://localhost:5000/api/auth';
+export const PERSONAL_INFO_URL = 'http://localhost:5000/api/personal-info';
+export const SUMMARY_URL = 'http://localhost:5000/api/summary';
+export const EXPERIENCE_URL = 'http://localhost:5000/api/work-experiences';
+export const EDUCATION_URL = 'http://localhost:5000/api/educations';
+export const SKILL_URL = 'http://localhost:5000/api/skills';
+export const PROJECT_URL = 'http://localhost:5000/api/projects';
+export const SOCIAL_URL = 'http://localhost:5000/api/social-links';
